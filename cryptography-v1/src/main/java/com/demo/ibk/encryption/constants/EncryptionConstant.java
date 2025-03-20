508b1899aa70cca925aaaee82a2c3fb9db35f3ea
Miguel Armas <marmasa@proveedor.intercorp.com.pe>
2025-03-20 17:30:15 -0500

    Add encryption constant

---
+++ cryptography-v1/src/main/java/com/demo/ibk/encryption/constants/EncryptionConstant.java
@@ -0,0 +1,11 @@
+package com.demo.ibk.encryption.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EncryptionConstant {
+
+    public static final String ENCRYPTION_METHOD_PARAM = "encryptionMethod";
+    public static final String FEATURE_PARAM = "encryptionMethod";
+}
--- cryptography-v1/src/main/java/com/demo/ibk/encryption/rest/EncryptionRestService.java
+++ cryptography-v1/src/main/java/com/demo/ibk/encryption/rest/EncryptionRestService.java
@@ -17,6 +17,9 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.demo.ibk.encryption.constants.EncryptionConstant.ENCRYPTION_METHOD_PARAM;
+import static com.demo.ibk.encryption.constants.EncryptionConstant.FEATURE_PARAM;
+
 @Slf4j
 @RequiredArgsConstructor
 @RestController
@@ -27,16 +30,16 @@ public class EncryptionRestService {
 
   @PostMapping("encrypt")
   public ResponseEntity<EncryptionResponseDto> encrypt(@Valid @RequestBody EncryptionRequestDto encryptionRequest,
-                                                       @RequestHeader(name = "encryptionMethod") String encryptionMethod,
-                                                       @RequestHeader(name = "feature") String feature) {
+                                                       @RequestHeader(name = ENCRYPTION_METHOD_PARAM) String encryptionMethod,
+                                                       @RequestHeader(name = FEATURE_PARAM) String feature) {
     EncryptionMethod method = EncryptionMethod.parse(encryptionMethod);
     return ResponseEntity.ok(service.encrypt(method, feature, encryptionRequest.getValue()));
   }
 
   @PostMapping("decrypt")
   public ResponseEntity<DecryptionResponseDto> decrypt(@Valid @RequestBody DecryptionRequestDto decryptionRequest,
-                                                       @RequestHeader(name = "encryptionMethod") String encryptionMethod,
-                                                       @RequestHeader(name = "feature") String feature) {
+                                                       @RequestHeader(name = ENCRYPTION_METHOD_PARAM) String encryptionMethod,
+                                                       @RequestHeader(name = FEATURE_PARAM) String feature) {
     EncryptionMethod method = EncryptionMethod.parse(encryptionMethod);
     return ResponseEntity.ok(service.decrypt(method, feature, decryptionRequest.getCipherMessage()));
   }
